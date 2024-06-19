7c0a8a5f5a37a845e7bb9ba790d638da7bc38bae
dnitecki <dominick.nitecki@outlook.com>
2024-06-18 22:12:41 -0400

    update sticky header

--- src/pages/overview/Overview.tsx
+++ src/pages/overview/Overview.tsx
@@ -4,9 +4,58 @@ export const Overview = () => {
   return (
     <div className="overview-container">
       <div className="overview-content">
-        <div className="card">
-          <h1>Overview</h1>
-        </div>
+        <section>
+          <div className="section-heading">
+            <h2>First Section</h2>
+          </div>
+          <div className="card">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim
+            nulla aliquet porttitor lacus luctus accumsan tortor posuere ac.
+            Dolor sit amet consectetur adipiscing elit pellentesque habitant
+            morbi tristique. Adipiscing commodo elit at imperdiet dui accumsan.
+            Urna id volutpat lacus laoreet non.
+          </div>
+        </section>
+        <section>
+          <div className="section-heading">
+            <h2>Second Section</h2>
+          </div>
+          <div className="card">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim
+            nulla aliquet porttitor lacus luctus accumsan tortor posuere ac.
+            Dolor sit amet consectetur adipiscing elit pellentesque habitant
+            morbi tristique. Adipiscing commodo elit at imperdiet dui accumsan.
+            Urna id volutpat lacus laoreet non.
+          </div>
+        </section>
+        <section>
+          <div className="section-heading">
+            <h2>Third Section</h2>
+          </div>
+          <div className="card">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim
+            nulla aliquet porttitor lacus luctus accumsan tortor posuere ac.
+            Dolor sit amet consectetur adipiscing elit pellentesque habitant
+            morbi tristique. Adipiscing commodo elit at imperdiet dui accumsan.
+            Urna id volutpat lacus laoreet non.
+          </div>
+        </section>
+        <section>
+          <div className="section-heading">
+            <h2>Fourth Section</h2>
+          </div>
+          <div className="card">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim
+            nulla aliquet porttitor lacus luctus accumsan tortor posuere ac.
+            Dolor sit amet consectetur adipiscing elit pellentesque habitant
+            morbi tristique. Adipiscing commodo elit at imperdiet dui accumsan.
+            Urna id volutpat lacus laoreet non.
+          </div>
+        </section>
       </div>
     </div>
   );
