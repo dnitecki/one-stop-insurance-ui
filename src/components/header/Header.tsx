c462a157430bdda940e24c41447480026ead5b5a
dnitecki <dominick.nitecki@outlook.com>
2024-06-21 19:34:44 -0400

    icons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,19 @@
 import "./Header.scss";
 import logo from "../../assets/OneStopIconWhite.png";
+import PlaceIcon from "@mui/icons-material/Place";
+import EmailIcon from "@mui/icons-material/Email";
+import CallIcon from "@mui/icons-material/Call";
 
 const Header = () => {
   return (
     <div className="header-container">
       <div className="header-logo">
         <img src={logo} />
+      </div>
+      <div className="header-options">
+        <CallIcon fontSize="inherit" />
+        <EmailIcon fontSize="inherit" />
+        <PlaceIcon fontSize="inherit" />
       </div>
     </div>
   );
