3b23acab1b977525505c783fb9eb41591688cefc
dnitecki <dominick.nitecki@outlook.com>
2024-06-21 00:13:39 -0400

    update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,11 @@
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div className="footer-container"></div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-content"></div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,14 @@
 import "./Header.scss";
+import logo from "../../assets/OneStopIconWhite.png";
 
 const Header = () => {
-  return <div className="header-container"></div>;
+  return (
+    <div className="header-container">
+      <div className="header-logo">
+        <img src={logo} />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
