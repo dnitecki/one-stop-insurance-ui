6b3641991053ad99bd80d8334a329a18bef20cbb
dnitecki <dominick.nitecki@outlook.com>
2024-06-27 20:56:04 -0400

    add ins companies

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -3,6 +3,8 @@ import Marquee from "react-fast-marquee";
 import erieLogo from "../../assets/erie-ins.png";
 import progressiveLogo from "../../assets/prog-ins.png";
 import travelersLogo from "../../assets/travelers-ins.png";
+import foremostLogo from "../../assets/foremost-ins.png";
+import openlyLogo from "../../assets/openly-ins.png";
 import { ServicesMapper } from "../../utils/servicesMapper";
 import { ServiceMapperType } from "../../types/types";
 
@@ -11,7 +13,7 @@ const Carousel = () => {
     <div className="scroller-container">
       <div className="scroller-header">
         <hr />
-        <h3>Partners</h3>
+        <h3>Representing</h3>
         <hr />
       </div>
       <Marquee gradient={true} gradientWidth={50}>
@@ -19,12 +21,14 @@ const Carousel = () => {
           <img src={progressiveLogo} className="progressive" />
           <img src={erieLogo} className="erie" />
           <img src={travelersLogo} className="travelers" />
+          <img src={foremostLogo} className="foremost" />
+          <img src={openlyLogo} className="openly" />
         </div>
       </Marquee>
       <Marquee gradient={true} gradientWidth={50} direction="right">
         <div className="scroller-services">
           {ServicesMapper.map((service: ServiceMapperType) => (
-            <div className="service-chip" key={service.id}>
+            <div className="service-chip" key={service.id} id={service.id}>
               <div className="service-icon">
                 <service.icon />
               </div>
