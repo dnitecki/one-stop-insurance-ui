ce74d883112e4f2e3201254fc01154fd61a576a8
dnitecki <dominick.nitecki@outlook.com>
2024-06-28 19:44:07 -0400

    fix dropdown

--- src/components/languageSelector/LanguageSelector.tsx
+++ src/components/languageSelector/LanguageSelector.tsx
@@ -4,45 +4,45 @@ import LanguageIcon from "@mui/icons-material/Language";
 import { LanguageEnumShort } from "../../enums/languageEnums";
 import { LanguageMapper } from "../../utils/languageMapper";
 import { LanguageMapperType } from "../../types/types";
+import { useCollapse } from "react-collapsed";
 
 const LanguageSelector = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isExpanded, setExpanded] = useState(false);
   const [language, setLanguage] = useState(LanguageEnumShort.EN);
+  const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
 
   const handleClick = () => {
-    setIsOpen(!isOpen);
+    setExpanded(!isExpanded);
   };
 
   const handleLanguageSelect = (language: LanguageEnumShort) => {
     setLanguage(language);
-    setIsOpen(!isOpen);
+    setExpanded(!isExpanded);
   };
   return (
     <div className="selector-container">
       <button
-        className={`selector-button ${isOpen ? "clicked" : ""}`}
-        onClick={handleClick}
+        className={`selector-button ${isExpanded ? "clicked" : ""}`}
+        {...getToggleProps({
+          onClick: () => handleClick(),
+        })}
       >
         <LanguageIcon fontSize="inherit" />
         <p>{language}</p>
       </button>
-      {isOpen ? (
-        <div className="selector-dropdown">
-          <>
-            {LanguageMapper.map((item: LanguageMapperType) => (
-              <button
-                className="dropdown-button"
-                key={item.id}
-                onClick={() => handleLanguageSelect(item.code)}
-              >
-                <p>{item.language}</p>
-              </button>
-            ))}
-          </>
+      <div className="selector-dropdown" {...getCollapseProps()}>
+        <div className="dropdown-content">
+          {LanguageMapper.map((item: LanguageMapperType) => (
+            <button
+              className="dropdown-button"
+              key={item.id}
+              onClick={() => handleLanguageSelect(item.code)}
+            >
+              <p>{item.language}</p>
+            </button>
+          ))}
         </div>
-      ) : (
-        <></>
-      )}
+      </div>
     </div>
   );
 };
