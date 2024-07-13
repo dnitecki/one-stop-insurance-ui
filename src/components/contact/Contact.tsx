5e894e0288fd3afcf4c720ad57b06ccc12bf8b1a
dnitecki <dominick.nitecki@outlook.com>
2024-07-13 19:51:16 -0400

    css changes

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,13 +1,43 @@
 import { useSearchParams } from "react-router-dom";
 import { SeachParamEnum } from "../../enums/languageEnums";
 import { ContentMapper } from "../../content/contentMapper";
+import "./Contact.scss";
+import FormControl from "@mui/material/FormControl/FormControl";
+import InputLabel from "@mui/material/InputLabel/InputLabel";
+import Select, { SelectChangeEvent } from "@mui/material/Select/Select";
+import MenuItem from "@mui/material/MenuItem/MenuItem";
+import { useState } from "react";
 
 const Contact = () => {
   const [searchParams] = useSearchParams();
   const language = searchParams.get(SeachParamEnum.LANG);
+  const [age, setAge] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
   return (
     <div className="card">
-      {ContentMapper.sections["contact"].body[language]}
+      <div className="contact-container">
+        <form className="form-container">
+          <FormControl fullWidth>
+            <InputLabel id="select-label">Age</InputLabel>
+            <Select
+              labelId="select-label"
+              id="simple-select"
+              value={age}
+              label="Age"
+              onChange={handleChange}
+              fullWidth
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </form>
+      </div>
     </div>
   );
 };
