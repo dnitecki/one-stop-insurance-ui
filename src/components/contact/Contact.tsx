0a8b2dcef7757ad4a1bdb736514de8fe722b54dd
dnitecki <dominick.nitecki@outlook.com>
2024-07-14 19:38:39 -0400

    form initialize

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,12 +2,7 @@ import { useSearchParams } from "react-router-dom";
 import { SeachParamEnum } from "../../enums/languageEnums";
 import { ContentMapper } from "../../content/contentMapper";
 import "./Contact.scss";
-import FormControl from "@mui/material/FormControl/FormControl";
-import InputLabel from "@mui/material/InputLabel/InputLabel";
-import Select, { SelectChangeEvent } from "@mui/material/Select/Select";
-import MenuItem from "@mui/material/MenuItem/MenuItem";
 import { useState } from "react";
-import { WHITE } from "../../constants/colors";
 import { EMPTY_STRING } from "../../constants/constants";
 
 const Contact = () => {
@@ -15,45 +10,38 @@ const Contact = () => {
   const language = searchParams.get(SeachParamEnum.LANG);
   const [policy, setProduct] = useState<string>(EMPTY_STRING);
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setProduct(event.target.value as string);
-  };
-
   return (
     <div className="card">
       <div className="contact-container">
         <form className="form-container">
-          <FormControl fullWidth>
-            <InputLabel
-              id="select-label"
-              sx={{
-                "&.Mui-focused": {
-                  color: WHITE,
-                },
-              }}
-            >
-              Policy Type
-            </InputLabel>
-            <Select
-              labelId="select-label"
-              id="simple-select"
-              value={policy}
-              label="Policy Type"
-              onChange={handleChange}
-              fullWidth
-              sx={{
-                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  borderColor: WHITE,
-                },
-              }}
-            >
-              <MenuItem value="auto">Auto</MenuItem>
-              <MenuItem value="home">Home</MenuItem>
-              <MenuItem value="life">Life</MenuItem>
-              <MenuItem value="business">Business</MenuItem>
-              <MenuItem value="other">Other</MenuItem>
-            </Select>
-          </FormControl>
+          <label htmlFor="fname">First Name</label>
+          <input type="text" id="fname" name="firstname" required />
+
+          <label htmlFor="lname">Last Name</label>
+          <input type="text" id="lname" name="lastname" required />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+          <label htmlFor="phone">Phone</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="(555) 555-5555"
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+            required
+          />
+          <label htmlFor="policy-type">Policy Type</label>
+          <select id="policy-type" defaultValue="Policy Type" required>
+            <option hidden selected>
+              Select one...
+            </option>
+            <option value="auto">Auto</option>
+            <option value="home">Home</option>
+            <option value="life">Life</option>
+            <option value="business">Business</option>
+            <option value="other">Other</option>
+          </select>
+          <input type="submit" value="Submit"></input>
         </form>
       </div>
     </div>
