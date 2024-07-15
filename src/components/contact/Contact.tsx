8537d303a721e04334c99e3b75553b333577f0e0
dnitecki <dominick.nitecki@outlook.com>
2024-07-14 21:40:59 -0400

    create form

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,6 +4,7 @@ import { ContentMapper } from "../../content/contentMapper";
 import "./Contact.scss";
 import { useState } from "react";
 import { EMPTY_STRING } from "../../constants/constants";
+import ContactForm from "../forms/ContactForm/ContactForm";
 
 const Contact = () => {
   const [searchParams] = useSearchParams();
@@ -13,36 +14,7 @@ const Contact = () => {
   return (
     <div className="card">
       <div className="contact-container">
-        <form className="form-container">
-          <label htmlFor="fname">First Name</label>
-          <input type="text" id="fname" name="firstname" required />
-
-          <label htmlFor="lname">Last Name</label>
-          <input type="text" id="lname" name="lastname" required />
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" required />
-          <label htmlFor="phone">Phone</label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            placeholder="(555) 555-5555"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            required
-          />
-          <label htmlFor="policy-type">Policy Type</label>
-          <select id="policy-type" defaultValue="Policy Type" required>
-            <option hidden selected>
-              Select one...
-            </option>
-            <option value="auto">Auto</option>
-            <option value="home">Home</option>
-            <option value="life">Life</option>
-            <option value="business">Business</option>
-            <option value="other">Other</option>
-          </select>
-          <input type="submit" value="Submit"></input>
-        </form>
+        <ContactForm />
       </div>
     </div>
   );
---
+++ src/components/forms/ContactForm/ContactForm.tsx
@@ -0,0 +1,74 @@
+import "./ContactForm.scss";
+
+const ContactForm = () => {
+  return (
+    <form className="form-container">
+      <div className="form-row">
+        <input
+          className="form-input"
+          type="text"
+          id="fname"
+          name="firstname"
+          placeholder="First Name"
+          required
+        />
+        <input
+          className="form-input"
+          type="text"
+          id="lname"
+          name="lastname"
+          placeholder="Last Name"
+          required
+        />
+      </div>
+      <div className="form-row">
+        <input
+          className="form-input"
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          required
+        />
+      </div>
+      <div className="form-row">
+        <input
+          className="form-input"
+          type="tel"
+          id="phone"
+          name="phone"
+          placeholder="Phone Number"
+          required
+        />
+
+        <select
+          className="form-select"
+          id="policy-type"
+          defaultValue="Policy Type"
+          required
+        >
+          <option hidden selected>
+            Policy Type
+          </option>
+          <option value="auto">Auto</option>
+          <option value="home">Home</option>
+          <option value="life">Life</option>
+          <option value="business">Business</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <textarea
+        className="form-additional-info"
+        id="additional-info"
+        rows={3}
+        wrap="on"
+        placeholder="Additional Information"
+      />
+      <button className="form-submit-btn" type="submit" value="Submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
