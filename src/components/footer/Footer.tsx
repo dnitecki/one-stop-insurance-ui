1a3493fcef7e35a9f3882c32d33bf3b29bf57520
dnitecki <dominick.nitecki@outlook.com>
2024-07-28 17:58:41 -0400

    add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,24 @@
 import "./Footer.scss";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <div className="footer-content"></div>
+      <div className="footer-content">
+        <div className="link-cluster">
+          <FacebookIcon fontSize="inherit" />
+          <InstagramIcon fontSize="inherit" />
+          <LinkedInIcon fontSize="inherit" />
+        </div>
+        <div className="footer-text">
+          <p>Made with &nbsp;</p>
+          <FavoriteIcon />
+          <p>&nbsp; by Dominick Nitecki</p>
+        </div>
+      </div>
     </div>
   );
 };
