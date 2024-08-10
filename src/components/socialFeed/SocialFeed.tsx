1aba1902ff94f37e540473d0ae147e8fea334a61
dnitecki <dominick.nitecki@outlook.com>
2024-08-10 16:48:19 -0400

    style changes

--- src/components/socialFeed/SocialFeed.tsx
+++ src/components/socialFeed/SocialFeed.tsx
@@ -1,8 +1,41 @@
+import { LINKS } from "../../constants/constants";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import Arrow from "../../assets/arrow.png";
 import "./SocialFeed.scss";
 
 const SocialFeed = () => {
   return (
     <div className="feed-container">
+      <div className="media-links">
+        <img className="left-arrow" src={Arrow} />
+        <a
+          className="link-container"
+          href={LINKS.FB}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FacebookIcon fontSize="inherit" />
+        </a>
+        <a
+          className="link-container"
+          href={LINKS.INSTA}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <InstagramIcon fontSize="inherit" />
+        </a>
+        <a
+          className="link-container"
+          href={LINKS.LINKEDIN}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedInIcon fontSize="inherit" />
+        </a>
+        <img className="right-arrow" src={Arrow} />
+      </div>
       <iframe
         width="100%"
         height="500"
