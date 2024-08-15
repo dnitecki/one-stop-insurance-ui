ec1b00b036b336cf3cf2c979b841fab686e6614f
dnitecki <dominick.nitecki@outlook.com>
2024-08-14 23:50:22 -0400

    add brokerage content

--- src/components/agency/Agency.tsx
+++ src/components/agency/Agency.tsx
@@ -1,6 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 import { SeachParamEnum } from "../../enums/languageEnums";
 import BeataHeadshot from "../../assets/BeataHeadshot.png";
+import BrokerIcon from "../../assets/BrokerIcon.png";
 import "./Agency.scss";
 import { ObjectIndexType } from "../../types/types";
 
@@ -8,7 +9,7 @@ const Agency = () => {
   const [searchParams] = useSearchParams();
   const language = searchParams.get(SeachParamEnum.LANG);
 
-  const content: ObjectIndexType = {
+  const experienceContent: ObjectIndexType = {
     EN: (
       <>
         Has more than <strong>30 years</strong> of combined insurance
@@ -28,17 +29,43 @@ const Agency = () => {
       </>
     ),
   };
+  const agencyContent: ObjectIndexType = {
+    EN: (
+      <>
+        Your <strong>One Stop</strong> Shop for smart savings and smarter
+        coverage.
+      </>
+    ),
+    PL: (
+      <>
+        <strong>Jedno</strong> miejsce na sprytne oszczędności i lepszą ochronę.
+      </>
+    ),
+    ES: (
+      <>
+        Tu lugar <strong>único</strong> para ahorros inteligentes y mejor
+        protección.
+      </>
+    ),
+  };
   return (
     <>
-      <div className="agency-card">
-        <div className="agency-text">
-          <h2>{content[language]}</h2>
+      <div className="experience-card">
+        <div className="experience-text">
+          <h2>{experienceContent[language]}</h2>
         </div>
         <div className="headshot-container">
           <img className="headshot" src={BeataHeadshot} alt="Beata Headshot" />
         </div>
       </div>
-      <div className="agency-card"></div>
+      <div className="agency-card">
+        <div className="icon-container">
+          <img className="broker-icon" src={BrokerIcon} alt="Beata Headshot" />
+        </div>
+        <div className="agency-text">
+          <h2>{agencyContent[language]}</h2>
+        </div>
+      </div>
     </>
   );
 };
