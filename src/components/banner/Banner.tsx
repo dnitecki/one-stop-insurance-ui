4dc6802d971ffae2cd77fb3d805c213d1721ef3c
dnitecki <dominick.nitecki@outlook.com>
2024-08-15 22:10:50 -0400

    add new photo

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,17 +1,11 @@
 import "./Banner.scss";
 import groupPhoto from "../../assets/GroupPhoto.png";
-import logo from "../../assets/OneStopLogo.png";
 
 const Banner = () => {
   return (
     <div className="banner-container">
-      <div className="banner-header">
-        <h1>ONE STOP</h1>
-        <h2>INSURANCE</h2>
-      </div>
       <div className="banner-content">
         <img className="group-photo" src={groupPhoto} alt="group photo" />
-        <img className="logo" src={logo} alt="one stop logo" />
       </div>
     </div>
   );
