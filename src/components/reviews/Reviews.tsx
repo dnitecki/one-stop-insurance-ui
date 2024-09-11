6dc957abdb2835dcd85cf43de1139eeb4bba1b68
dnitecki <dominick.nitecki@outlook.com>
2024-09-10 21:02:07 -0400

    css

--- src/components/reviews/Reviews.tsx
+++ src/components/reviews/Reviews.tsx
@@ -5,37 +5,52 @@ import "./Reviews.scss";
 import StarIcon from "@mui/icons-material/Star";
 import { SeachParamEnum } from "../../enums/languageEnums";
 import { ContentMapper } from "../../content/contentMapper";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Reviews = () => {
   const [searchParams] = useSearchParams();
   const language = searchParams.get(SeachParamEnum.LANG);
   const content = ContentMapper?.sections?.reviews?.body[language];
   const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [isFading, setIsFading] = useState<boolean>(true);
+  const ReviewCard = useRef(null);
+
   useEffect(() => {
-    if (activeIndex < ReviewMapper.length) {
+    const fadeInDuration = 500;
+    const fadeOutDuration = 500;
+    const displayDuration = 4000;
+
+    const timer = setTimeout(() => {
+      setIsFading(false); // Start fading out
       setTimeout(() => {
-        setActiveIndex(activeIndex + 1);
-      }, 4000);
-    }
-    if (activeIndex >= ReviewMapper.length - 1) {
-      setTimeout(() => {
-        setActiveIndex(0);
-      }, 4000);
-    }
-  });
+        setActiveIndex((prevIndex) => (prevIndex + 1) % ReviewMapper.length);
+        setTimeout(() => {
+          setIsFading(true); // Start fading in
+        }, fadeInDuration);
+      }, fadeOutDuration);
+    }, displayDuration);
+
+    return () => clearTimeout(timer);
+  }, [activeIndex]);
+
   const activeReview = ReviewMapper[activeIndex];
+
   return (
     <div className="reviews-container">
-      <div className="review-card" key={activeReview?.name}>
-        <div className="review-rating">
-          <StarIcon className="star-icon" fontSize="inherit" />
-          <h3>{activeReview?.rating.toFixed(1)}</h3>
-        </div>
-        <div className="review-content">
-          <p>{activeReview?.review}</p>
-          <div className="review-name">
-            <h3>-{activeReview?.name}</h3>
+      <div className="review-container">
+        <div
+          className={`review-card ${isFading && "fade-in"}`}
+          key={activeReview?.name}
+        >
+          <div className="review-rating">
+            <StarIcon className="star-icon" fontSize="inherit" />
+            <h3>{activeReview?.rating.toFixed(1)}</h3>
+          </div>
+          <div className="review-content">
+            <p>{activeReview?.review}</p>
+            <div className="review-name">
+              <h3>-{activeReview?.name}</h3>
+            </div>
           </div>
         </div>
       </div>
