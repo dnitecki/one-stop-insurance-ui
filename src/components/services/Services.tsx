68d533ab2f125180f42042064dc33af8ae3f353b
dnitecki <dominick.nitecki@outlook.com>
2025-05-05 19:34:58 -0400

    styling details

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -7,23 +7,21 @@ const MainServices = ServicesMapper;
 const Services = () => {
   return (
     <div className="services-container">
-      <div className="services-card">
-        <div className="services-content">
-          {MainServices.map((service: ServiceMapperType) => (
-            <div className="service-chip" key={service.id} id={service.id}>
-              <div className="content-container">
-                <div className="service-icon">
-                  <div className="icon">
-                    <service.icon fontSize="inherit" />
-                  </div>
-                </div>
-                <div className="service-name">
-                  <p>{service.text}</p>
+      <div className="services-content">
+        {MainServices.map((service: ServiceMapperType) => (
+          <div className="service-chip" key={service.id} id={service.id}>
+            <div className="content-container">
+              <div className="service-icon">
+                <div className="icon">
+                  <service.icon fontSize="inherit" />
                 </div>
               </div>
+              <div className="service-name">
+                <p>{service.text}</p>
+              </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
