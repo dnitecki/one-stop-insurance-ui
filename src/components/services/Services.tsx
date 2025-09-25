ffd2c9a827d9424aec8933e204add45f41b0467d
dnitecki <dominick.nitecki@outlook.com>
2025-09-25 00:14:55 -0400

    Style and content touchups

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,18 +1,29 @@
 import "./Services.scss";
+import { useRef } from "react";
 import { ServicesMapper } from "../../mappers/servicesMapper";
 import { ServiceMapperType } from "../../types/types";
 import useTranslateContent from "../../hooks/useTranslateContent";
 import { ContentSectionEnum } from "../../enums/enums";
+import useVisibility from "../../hooks/useVisibility";
+import { EMPTY_STRING } from "../../constants/constants";
 
 const Services = () => {
   const MainServices = ServicesMapper;
   const { content } = useTranslateContent(ContentSectionEnum.SERVICES);
+  const ref = useRef<HTMLLIElement>(null);
+  const isVisible = useVisibility(ref);
 
   return (
     <div className="services-container">
       <ul className="services-content">
-        {MainServices.map((service: ServiceMapperType) => (
-          <li className="service-chip" key={service.id} id={service.id}>
+        {MainServices.map((service: ServiceMapperType, index) => (
+          <li
+            className={`service-chip ${isVisible ? "visible" : EMPTY_STRING}`}
+            key={service.id}
+            id={service.id}
+            ref={ref}
+            style={{ "--delay": `${index * 0.1}s` } as React.CSSProperties}
+          >
             <div className="content-container">
               <div className="service-icon">
                 <div className="icon">
